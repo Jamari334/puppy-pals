58e31ff39c76f83a20b3cdba5cc26e61735bc91f
King Gambit Lawrence <kinggambitlawrence@kings-air.mynetworksettings.com>
2023-12-15 10:20:33 -0500

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-
+import { puppyList } from './data.js'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+  const [singlePupId, setSinglePupId] = useState('null')
   
-
+  const handleClick = () =>{
+    {()=>{console.log("puppy id: ", puppy.id)}}
+  }
+  
+  
+  console.log('puppy list:', puppies)
   return (
     <>
-      
+      <div className='App'>
+        {
+          puppies.map((puppy) => {
+            return <p onClick={()=>{handleClick}} key={puppy.id}>{puppy.name}</p>
+          })
+        }
+      </div>
     </>
   )
 }
